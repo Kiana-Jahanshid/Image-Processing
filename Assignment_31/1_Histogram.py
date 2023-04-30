e9a102a756533e7464082714c07e1785b62018e6
kiana-jahanshid <64160703+kiana-jahanshid@users.noreply.github.com>
2023-04-30 11:52:25 +0330

    Update 1_Histogram.py

--- Assignment_31/1_Histogram.py
+++ Assignment_31/1_Histogram.py
@@ -7,42 +7,23 @@ image  = cv2.imread("inputs/Fruit.jpg" , cv2.IMREAD_GRAYSCALE)
 rows , cols = image.shape
 print(image.shape)
 
+def histogram_calculation(image) :
+    histogram = np.zeros(256)
+    for i in range(rows):
+        for j in range(cols):
+            value = image[i , j]
+            histogram[value] += 1 
+    return histogram
 
-histogram = np.zeros(256)
-for i in range(rows):
-    for j in range(cols):
+#1️⃣
+histogram = histogram_calculation(image)
+plt.plot(histogram)
 
-        value = image[i , j]
-        histogram[value] += 1 
-#plt.plot(histogram)
+#2️⃣
+y = np.arange(256)
+#plt.bar( y ,  histogram )
 
+#3️⃣
+#plt.hist( image.ravel() , 256)
 
-
-
-
-values = []
-count = []
-for i in range(len(histogram)):
-    values.append(i)
-    count.append(histogram[i])
-y = np.arange(len(values))
-#plt.bar( y ,  count )
-
-
-
-h = np.zeros(256)
-for i in range(rows):
-    for j in range(cols):
-
-        v = image[i , j]
-        h[v] += 1 
-
-
-
-plt.hist( value  , y   , range=(0,256))
 plt.show()
-
-# image=cv2.resize(image, (600 , 800))
-# cv2.imshow("",histogram)
-# cv2.waitKey()
-
