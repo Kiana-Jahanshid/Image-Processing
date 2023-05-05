2d857331f90d4d55ad105a7d3c902283f79044ea
kiana-jahanshid <64160703+kiana-jahanshid@users.noreply.github.com>
2023-05-06 02:16:40 +0330

    2nd

--- Assignment_32/2_median_filter.py
+++ Assignment_32/2_median_filter.py
@@ -3,15 +3,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-img = cv2.imread("inputs/board.tif" , cv2.IMREAD_GRAYSCALE )
+img = cv2.imread("inputs/xray.jpg" , cv2.IMREAD_GRAYSCALE )
 
 x = img
-for i in range(5) :
+for i in range(4) :
     x = cv2.medianBlur(x , 3 )
+x = cv2.medianBlur(x , 9 )
 
 horizontal = np.hstack((img , x))
 final_concat = np.concatenate((img , x), axis=1)
-cv2.imwrite("outputs/board_MEDIAN_filter.jpg" ,final_concat )
+
+cv2.imwrite("outputs/xray_MEDIAN_filter.jpg" ,final_concat )
 cv2.imshow('MEDIAN filter', final_concat)
 cv2.waitKey()
 
