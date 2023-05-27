3ae89309254df657e00c0a6717ba512b05a19c4e
kiana-jahanshid <64160703+kiana-jahanshid@users.noreply.github.com>
2023-05-27 13:23:50 +0330

    Update ColorDetection.py

--- Assignment_35/ColorDetection.py
+++ Assignment_35/ColorDetection.py
@@ -11,16 +11,14 @@ upper = {'red':(186,255,255) , 'green':(86,255,255)  , 'blue':(117,255,255) , 'y
 colors= {'red':(0,0,255)     , 'green':(0,255,0)     , 'blue':(255,0,0)     , 'yellow':(0, 255, 217), 'orange':(0,140,255) , 'purple':(128, 0, 128)}
 
 
-mask_yellow = cv2.inRange(frame, lower['yellow'], upper['yellow'])
-mask_orange = cv2.inRange(frame, lower['orange'], upper['orange'])
 
-temp =[]
-w=[]
+black =[]
+white=[]
 for i in range(20):
-    temp.append(i)
+    black.append(i)
 
 for i in range(200 , 256):
-    w.append(i)
+    white.append(i)
 
 while True:
     _ , frame = cap.read()
@@ -34,34 +32,38 @@ while True:
     g_mean = np.mean(g)
     r_mean = np.mean(r)
 
-    # for i in range(rows):
-    #     for j in range(cols):
-    for pixel in mask :
-            
-            # if  pixel.all() in temp :
-            #     cv2.putText(frame , "black " , (50 , 70) , cv2.FONT_HERSHEY_SIMPLEX , 1 , 6 , 3)
-                
-            if pixel.all() in w:
-                cv2.putText(frame , "white " , (50 , 130) , cv2.FONT_HERSHEY_SIMPLEX , 1 , 6 , 3)     
 
-            # elif mask[i,j][0] == colors["yellow"][0]  and mask[i,j][1] == colors["yellow"][1] and mask[i,j][2] == colors["yellow"][2]:
-            #     cv2.putText(frame , "yellow " , (50 , 150) , cv2.FONT_HERSHEY_SIMPLEX , 1 , 6 , 3)
+    for i in range(rows):
+        for j in range(cols):   
 
-            # elif mask[i,j][0] == colors["purple"][0]  and mask[i,j][1] == colors["purple"][1] and mask[i,j][2] == colors["purple"][2] :
-            #     cv2.putText(frame , "purple " , (50 , 170) , cv2.FONT_HERSHEY_SIMPLEX , 1 , 6 , 3)
 
-            # elif mask[i,j][0] == colors["orange"][0]  and mask[i,j][1] == colors["orange"][1] and mask[i,j][2] == colors["orange"][2] :
-            #     cv2.putText(frame , "orange " , (50 , 190) , cv2.FONT_HERSHEY_SIMPLEX , 1 , 6 , 3)    
-
-            elif (g_mean > r_mean and g_mean > b_mean):
+            if (g_mean > r_mean and g_mean > b_mean):
                 cv2.putText(frame , "GREEN " , (50 , 90) , cv2.FONT_HERSHEY_SIMPLEX , 1 , 6 , 3)
             elif (r_mean > b_mean and r_mean > g_mean):
-                cv2.putText(frame , "RED "   , (250 , 90) , cv2.FONT_HERSHEY_SIMPLEX , 1 , 6 , 3)
+                cv2.putText(frame , "RED "   , (250 , 90), cv2.FONT_HERSHEY_SIMPLEX , 1 , 6 , 3)
             elif (b_mean > g_mean and b_mean > r_mean):
                 cv2.putText(frame , "BLUE "  , (450 , 90), cv2.FONT_HERSHEY_SIMPLEX , 1 , 6 , 3)
-      
 
+            elif  mask[i,j].all() in black :
+                cv2.putText(frame , "black " , (50 , 70) , cv2.FONT_HERSHEY_SIMPLEX , 1 , 6 , 3)
+                
+            elif mask[i,j].all() in white:
+                cv2.putText(frame , "white " , (50 , 130), cv2.FONT_HERSHEY_SIMPLEX , 1 , 6 , 3) 
+
+            elif (mask[i,j][0] >= lower["yellow"][0]  and mask[i,j][1] >= lower["yellow"][1] and mask[i,j][2] >= lower["yellow"][2] ) \
+            or (mask[i,j][0] <= upper["yellow"][0]  and mask[i,j][1] <= upper["yellow"][1] and mask[i,j][2] <= upper["yellow"][2] ):
+                cv2.putText(frame , "yellow " , (50 , 190) , cv2.FONT_HERSHEY_SIMPLEX , 1 , 6 , 3)             
+                
+            elif (mask[i,j][0] >= lower["purple"][0]  and mask[i,j][1] >= lower["purple"][1] and mask[i,j][2] >= lower["purple"][2] ) \
+            or (mask[i,j][0] <= upper["purple"][0]  and mask[i,j][1] <= upper["purple"][1] and mask[i,j][2] < upper["purple"][2] ):
+                cv2.putText(frame , "purple " , (50 , 170) , cv2.FONT_HERSHEY_SIMPLEX , 1 , 6 , 3)
+
+            elif (mask[i,j][0] >= lower["orange"][0]  and mask[i,j][1] >= lower["orange"][1] and mask[i,j][2] >= lower["orange"][2] ) \
+            or (mask[i,j][0] <= upper["orange"][0]  and mask[i,j][1] <= upper["orange"][1] and mask[i,j][2] <= upper["orange"][2] ):
+                cv2.putText(frame , "orange " , (50 , 190) , cv2.FONT_HERSHEY_SIMPLEX , 1 , 6 , 3) 
  
+
+
     cv2.imshow("result" , frame)
     if cv2.waitKey(25) & 0xFF == ord("b"):
         break
