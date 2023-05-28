8df5e7a1db5c54dab3ea7efdfbcb4e654d9979a9
kiana-jahanshid <64160703+kiana-jahanshid@users.noreply.github.com>
2023-05-28 12:07:33 +0330

    Update My_PNG_converter.py

--- Assignment_35/My_PNG_converter.py
+++ Assignment_35/My_PNG_converter.py
@@ -6,9 +6,9 @@ img = img.convert("RGBA")
 image = np.array(img)
 
 gray = np.sum(image[:,:,:3], axis=2)
-white_mask = np.where(gray == 80*3, 1, 0)
+gray_mask = np.where(gray == 80*3, 1, 0)
 
-alpha = np.where(white_mask, 0, image[:,:,-1])
+alpha = np.where(gray_mask, 0, image[:,:,-1])
 
 image[:,:,-1] = alpha 
 img = Image.fromarray(np.uint8(image))
